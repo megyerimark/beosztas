ff5568f5cc55a3f8d0f70c400e8e6e6c475aa4e9
unknown <megyko1234@gmail.com>
2022-05-31 10:03:37 +0200

    Javítva az app.java

--- src/App.java
+++ src/App.java
@@ -1,3 +1,16 @@
+/*
+* File: App
+.java
+* Author:Megyeri Márk Máté
+* Copyright: 2022, Megyeri Márk Máté 
+* Group: Szoft I/N
+* Date: 2022-05-31
+* Github: https://github.com/megyerimark/
+* Licenc: GNU GPL
+*/
+
+
+
 import controllers.MainController;
 
 public class App {
--- src/views/MainFrame.java
+++ src/views/MainFrame.java
@@ -21,7 +21,7 @@ import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JScrollPane;
+
 
 public class MainFrame extends JFrame {
     JPanel mainPanel;
